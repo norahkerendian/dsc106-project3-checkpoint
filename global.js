5e449111f4b2ffb0188fb42a8184732f0811707c
norahkerendian <nokerendian@ucsd.edu>
2025-02-09 17:48:47 -0800

    line plot, plotted

--- global.js
+++ global.js
@@ -112,3 +112,77 @@ var plot=function(filePath){
     });
 
 }
+
+d3.json("dengue_cases.json").then(function(data) {
+    // Set up SVG dimensions
+    const margin = {top: 40, right: 100, bottom: 50, left: 60};
+    const width = 900 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Parse data
+    const municipalities = d3.groups(data, d => d.Municipality);
+    
+    // Scales
+    const x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.Year))
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Cases)])
+        .nice()
+        .range([height, 0]);
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10)
+        .domain(municipalities.map(d => d[0]));
+
+    // Line generator
+    const line = d3.line()
+        .x(d => x(d.Year))
+        .y(d => y(d.Cases));
+
+    // Draw lines for each municipality
+    svg.selectAll(".line")
+        .data(municipalities)
+        .enter()
+        .append("path")
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", d => color(d[0]))
+        .attr("stroke-width", 2)
+        .attr("d", d => line(d[1]));
+
+    // X-axis
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+
+    // Y-axis
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Legend
+    const legend = svg.append("g")
+        .attr("transform", `translate(${width + 10},0)`);
+    
+    municipalities.forEach((d, i) => {
+        legend.append("circle")
+            .attr("cx", 10)
+            .attr("cy", i * 20)
+            .attr("r", 5)
+            .style("fill", color(d[0]));
+
+        legend.append("text")
+            .attr("x", 20)
+            .attr("y", i * 20 + 5)
+            .text(d[0])
+            .style("font-size", "12px")
+            .attr("alignment-baseline", "middle");
+    });
+
+});
